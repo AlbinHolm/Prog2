3fb4e674c9a1ef6e9890ccfedd96e5d646555a5f
Albin-dev <albin.holm@skola.taby.se>
2021-09-23 08:49:34 +0200

    Uppgift 1.4 Addition och Sojamjölk - 2021-09-23

--- upg1_4.py
+++ upg1_4.py
@@ -1,3 +1,4 @@
+#Additionsfunktion
 def addition(*val):
     summa = 0
     for arg in val:
@@ -9,3 +10,14 @@ def caller(func,val):
 
 
 print(caller(addition, (7, 4, 7, 2)))
+
+#Funktion för uppgift "Sojamjölk"
+def food(s, vegan=False):
+    if vegan:
+        print("Sojamjölk")
+    else:
+        print("Mjölk")
+
+
+food("Mjölk", False)
+food("Mjölk", True)
