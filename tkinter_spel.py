8a83303f0ba0a6d127c8d4c1cf6faafd5a85deed
Albin-dev <albin.holm@skola.taby.se>
2022-01-27 09:06:53 +0100

    Tkinter spel klart (2022-01-27)

--- tkinter_spel.py
+++ tkinter_spel.py
@@ -3,13 +3,18 @@ import random
 
 root = Tk()
 canv = Canvas(root, height=400, width=400, bg="blue")
+
+score = Label(canv)
+score.place(x = 10, y = 10)
+score["text"] = "Score: 0"
+scoreint = 0
+
 canv.pack()
 img2 = PhotoImage(file = "media/duck.png")
 
 img = PhotoImage(file = "media/crosshairs.png")
 id = canv.create_image(200, 150, image=img)
 movespeed = 10
-score = 0
 
 x_pos = 0
 y_pos = 0
@@ -27,25 +32,38 @@ def updatePos():
     global y_pos
     x_pos = (canv.coords(id)[0] - 3)
     y_pos = (canv.coords(id)[1] - 12)
-    # print("Debug: Updated pos")
+
 
 # Picks a new random pos for target
 def updateRandPos():
     global randPosX
     global randPosY
-    randPosX = random.randint(17, 377)
-    randPosY = random.randint(8, 210)
-    # print("Debug: Updated randpos")
+    randPosX = random.randint(-50, 50)
+    randPosY = random.randint(-50, 50)
+
 
 # Moves target to updated pos
 def updateTarget():
     updateRandPos()
-    canv.move(target, randPosX + 5, randPosY + 15)
-    # print("Debug: Updated target")
+    canv.move(target, randPosX, randPosY)
+
+    # If target moves out of screen
+    if canv.coords(target)[0] > 300:
+        canv.move(target, -100, 0)
+
+    if canv.coords(target)[0] < 10:
+        canv.move(target, 100, 0)
+    
+    if canv.coords(target)[1] > 300:
+        canv.move(target, 0, -100)
+            
+    if canv.coords(target)[1] < 10:
+        canv.move(target, 0, 100)
+
 
 
 updateRandPos()
-target = canv.create_image(randPosX + 5, randPosY + 15, image=img2)
+target = canv.create_image(200, 200, image=img2)
 
 
 def move_left(e):
@@ -68,38 +86,34 @@ def shoot(e):
     global xHit
     global yHit
     global score
+    global scoreint
 
     updatePos()
     
-    if (abs(x_pos - randPosX) <= 15):
+    if (abs(x_pos - canv.coords(target)[0]) <= 15):
         xHit = True
-    if (abs(y_pos - randPosY) <= 15):
+    if (abs(y_pos - canv.coords(target)[1]) <= 15):
         yHit = True
 
     if (xHit == True & yHit == True):
         shot = canv.create_oval(0, 10, 5, 15, fill="red")
         canv.move(shot, x_pos, y_pos)
-        
-        print("hit")
+
+        scoreint += 1
+        score["text"] = "Score: " + str(scoreint)
+    
         updateTarget()
-        score += 1 
-        
-        
+
     else:
-        print("miss")
+        scoreint -= 1
+        score["text"] = "Score: " + str(scoreint)
 
     xHit = False
     yHit = False
 
-def debug(e):
-    global x_pos
-    global y_pos
-    global randPosX
-    global randPosY
 
-    updatePos()
-    print(x_pos, y_pos)
-    print(randPosX, randPosY)
+def debug(e):
+    pass
 
 
 
@@ -111,11 +125,3 @@ canv.bind_all("<space>", shoot)
 canv.bind_all("<8>", debug) 
 
 root.mainloop()
-
-
-
-
-
-
-# Fixa anka spawnar utanför canvas
-# Update anka och ta bort
